2aa35f5f0ee2c475bc51d6682d0e2a8047dc5b6d
Inzaghi's Dev <139905564+inzaghidev@users.noreply.github.com>
2024-02-12 21:30:00 +0700

    Add files via upload

--- public_html/pages/sitemap.php
+++ public_html/pages/sitemap.php
@@ -1,65 +1,75 @@
 <?php
-	$page_title = "Sitemap";
-	include('../layouts/header.php');
+    $page_title = "Sitemap";
+    include('../layouts/header.php');
 ?>
-    <section class="sitemap">
-      <div class="welcome-container">
-        <div class="welcome-title">
-          <h1 class="welcome-title">SITEMAP</h1>
-        </div>
-        <div class="welcome-wrapper">
-          <div class="welcome-to-inzaghis-sites">
-            <div class="welcome-description">
-              <ul id="sitemap"></ul>
-              <?php
-                function generateSitemap($dir, $pathToWebRoot = '') {
-                    $handle = opendir($dir);
-                    echo "<ul>";
+<section class="sitemap">
+  <div class="container-top"></div>
+  <div class="welcome-container">
+    <div class="welcome-title">
+      <h1 class="welcome-title">SITEMAP</h1>
+    </div>
+    <div class="welcome-wrapper">
+      <div class="welcome-to-inzaghis-sites">
+        <div class="welcome-description">
+          <ul id="sitemap">
+            <?php
+              function generateSitemap($dir, $pathToWebRoot = '', $level = 0) {
+                  $handle = opendir($dir);
 
-                    while (($file = readdir($handle)) !== false) {
-                        if ($file != "." && $file != "..") {
-                            $currentPath = $dir . '/' . $file;
-                            $relativePath = $pathToWebRoot . $file;
+                  if ($handle === false) {
+                      return; // Jika opendir gagal, hentikan fungsi
+                  }
 
-                            echo "<li>";
+                  while (($file = readdir($handle)) !== false) {
+                      if ($file != "." && $file != "..") {
+                          $currentPath = $dir . '/' . $file;
+                          $relativePath = $pathToWebRoot . $file;
 
-                            if (is_dir($currentPath)) {
-                                // If it's a directory, make it a clickable link
-                                echo "<a href='$relativePath'>$file</a>";
-                                // Recursive call for subdirectories
-                                generateSitemap($currentPath, $relativePath . '/');
-                            } else {
-                                // If it's a file, just display the file name
-                                echo $file . '<br/>';
-                            }
+                          echo "<li>";
 
-                            echo "</li>";
-                        }
-                    }
+                          if (is_dir($currentPath)) {
+                              // If it's a directory, make it a clickable link
+                              echo str_repeat("&nbsp;", $level * 4) . "├── <a href='$relativePath'>$file/</a><br/>";
+                              // Recursive call for subdirectories
+                              generateSitemap($currentPath, $relativePath . '/', $level + 1);
+                          } else {
+                              // If it's a file, just display the file name
+                              echo str_repeat("&nbsp;", $level * 4) . "├── <a href='$relativePath'>$file</a><br/>";
+                          }
 
-                    echo "</ul>";
-                    closedir($handle);
-                }
+                          echo "</li>";
+                      }
+                  }
 
-                // Set the root directory
-                $webRoot = realpath(dirname(__DIR__));
-                $serverRoot = realpath($_SERVER['DOCUMENT_ROOT']);
-                if ($webRoot === $serverRoot) {
-                    $pathToWebRoot = "";
-                } else {
-                    $pathToWebRoot = substr($webRoot, strlen($serverRoot) + 1);
-                }
+                  closedir($handle);
+              }
 
-                // Explicitly specify the root directory for sitemap generation
-                generateSitemap($webRoot, $pathToWebRoot);
-              ?>
+              // Set the root directory
+              $webRoot = realpath(dirname(__DIR__));
+              $serverRoot = realpath($_SERVER['DOCUMENT_ROOT']);
+              if ($webRoot === $serverRoot) {
+                  $pathToWebRoot = "";
+              } else {
+                  $pathToWebRoot = substr($webRoot, strlen($serverRoot) + 1);
+              }
 
-              <script src="../js/script.js"></script>
-            </div>
-          </div>
+              // Explicitly specify the root directory for sitemap generation
+              echo "<ul>";
+              echo "<li><strong>📁 public</strong></li>";
+              generateSitemap($webRoot . '/public/', '', 1);
+              echo "<li><strong>📁 page-apps</strong></li>";
+              generateSitemap($webRoot . '/page-apps/', '', 1);
+              echo "<li><strong>📁 pages</strong></li>";
+              generateSitemap($webRoot . '/pages/', '', 1);
+              echo "</ul>";
+            ?>
+          </ul>
+          <script src="../js/script.js"></script>
         </div>
       </div>
-    </section>
+    </div>
+  </div>
+</section>
 <?php
-	include('../layouts/footer.php');
+    include('../layouts/footer.php');
 ?>
