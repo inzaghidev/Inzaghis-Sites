b9ea5100e9c25613e8992576d42e957c7c4e3164
inzaghidev <izzumiposhaf29@gmail.com>
2024-12-24 14:46:49 +0700

    Edited some Files

--- isites-php/pages/apps.php
+++ isites-php/pages/apps.php
@@ -1,19 +1,21 @@
 <?php
-  $page_title = "Apps";
-  include('../layouts/header.php');
-  include('../components/navbar/navbar.php');
-  $data = json_decode(file_get_contents('../data/apps-data.json'), true);
 
-  if (isset($data['apps'])) {
-      $apps = $data['apps'];
-  } else {
-      $apps = []; // Atau tangani kesalahan dengan lebih baik sesuai kebutuhan Anda
-  }
+  include('../page-apps/index.php');
+
+  // $page_title = "Apps";
+  // include('../layouts/header.php');
+  // include('../components/navbar/navbar.php');
+  // $data = json_decode(file_get_contents('../data/apps-data.json'), true);
+
+  // if (isset($data['apps'])) {
+  //     $apps = $data['apps'];
+  // } else {
+  //     $apps = []; // Atau tangani kesalahan dengan lebih baik sesuai kebutuhan Anda
+  // }
 ?>
-<div class="container-top"></div>
-<section class="pages-background">
+<!-- <div class="container-top"></div> -->
+<!-- <section class="pages-background">
   <section class="pages">
-    <!-- Page Heading -->
     <h1 class="my-4 title-page text-4xl font-bold text-left">
       Pages Apps <small class="text-lg text-gray-500">Beta</small>
     </h1>
@@ -163,41 +165,12 @@
           </div>
         </div>
     </div>
-      <!-- -->
     </div>
-    <div class="row" id="app-container">
-    <?php
-    //  // Loop melalui setiap aplikasi dan buat elemen HTML-nya
-    //  foreach ($apps as $app) {
-    //    // Menggunakan isset untuk menghindari error jika kunci tidak ada
-    //    $name = isset($app['name']) ? $app['name'] : 'N/A';
-    //    $imgSrc = isset($app['imgSrc']) ? $app['imgSrc'] : '';
-    //    $description = isset($app['description']) ? $app['description'] : (isset($app['text']) ? $app['text'] : '');
-    //    $link = isset($app['link']) ? $app['link'] : '#';
-    //    ?>
-      <!--div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-          <div class="card h-100 border-success shadow">
-              <a href="<?php echo $link; ?>"><img class="card-img-top" src="<?php echo $imgSrc; ?>" alt=""></a>
-              <div class="card-body card-apps-body">
-                  <div class="card-apps">
-                      <h4 class="card-title text-title card-name page-apps-card"><a><?php echo $name; ?></a></h4>
-                      <p class="card-text"><?php echo $description; ?></p>
-                  </div>
-                  <button type="button" class="btn btn-primary btn-md apps-button"><a href="<?php echo $link; ?>" class="blog-link">Click here</a></button>
-              </div>
-          </div>
-      </div-->
-      <?php
-    //  }
-    //  ?>
-    </div>
-
-    <!-- /.row -->
   </section>
-</section>
+</section> -->
 <?php
-  include('../components/footsite/footsite.php');
-  include('../layouts/footer.php');
+  // include('../components/footsite/footsite.php');
+  // include('../layouts/footer.php');
 ?>
 
 <!-- https://preline.co/examples/icon-sections.php -->
