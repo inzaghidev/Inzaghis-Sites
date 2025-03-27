2860a00cdc2eb010a7f833df8e11f59345070ba1
inzaghidev <izzumiposhaf29@gmail.com>
2025-03-27 16:55:36 +0700

    Edited and Improved on Footsite (Footer)

--- isites-php/components/footsite/footsite.php
+++ isites-php/components/footsite/footsite.php
@@ -6,60 +6,78 @@
                     <img src="<?php echo $pathToWebRoot.'/assets/icons/inzaghis-corp-logo-vertical-transparent.png'; ?>" class="h-16 w-auto" alt="logo" />
                 </a>
             </section>
-            <section class="special-pages">
-                <section class="footer-title mb-6 text-xl sm:text-sm lg:text-lg font-semibold text-black uppercase">Special Pages</section>
-                <nav>
-                    <ul>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/about.php'; ?>" class="text-gray-800 hover:text-blue-800">About</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/disclaimer.php'; ?>" class="text-gray-800 hover:text-blue-800">Disclaimer</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/privacy-policy.php'; ?>" class="text-gray-800 hover:text-blue-800">Privacy Policy</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/contact.php'; ?>" class="text-gray-800 hover:text-blue-800">Contact</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/portals/'; ?>" class="text-gray-800 hover:text-blue-800">Portals</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/'; ?>" class="text-gray-800 hover:text-blue-800">Apps</a></li>
-                        <li><a href="https://medium.com/@izzumiposhaf29" class="text-gray-800 hover:text-blue-800" target="_blank">Blog</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/sitemap.php'; ?>" class="text-gray-800 hover:text-blue-800">Sitemap</a></li>
-                    </ul>
-                </nav>
-            </section>
-            <section class="apps-tools">
-                <section class="footer-title mb-6 text-xl sm:text-sm lg:text-lg font-semibold text-black uppercase">Apps & Tools</section>
-                <nav>
-                    <ul>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/converter.php'; ?>" class="text-gray-800 hover:text-blue-800">Converters</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/calculators.php'; ?>" class="text-gray-800 hover:text-blue-800">Calculators</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/generators.php'; ?>" class="text-gray-800 hover:text-blue-800">Generators</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/formatters.php'; ?>" class="text-gray-800 hover:text-blue-800">Formatters</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/file-converter.php'; ?>" class="text-gray-800 hover:text-blue-800">File Converter</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/utilities.php'; ?>" class="text-gray-800 hover:text-blue-800">Utilities</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/tester-tools.php'; ?>" class="text-gray-800 hover:text-blue-800">Tester Tools</a></li>
-                    <li><a href="<?php echo $pathToWebRoot.'/page-apps/'; ?>" class="text-gray-800 hover:text-blue-800">Other Tools</a></li>
-                    </ul>
-                </nav>
-            </section>
-            <section class="network">
-                <section class="footer-title mb-6 text-xl sm:text-sm lg:text-lg font-semibold text-black uppercase">Networks</section>
-                <nav>
-                    <ul>
-                        <li><a href="<?php echo $pathToWebRoot.'/inzaghis-group/inzaghis-blog.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Blog</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/inzaghis-group/inzaghis-media.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Media</a></li>
-                        <li><a href="https://github.com/inzaghidev" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Dev (GitHub)</a></li>
-                        <li><a href="https://sites.google.com/view/inzaghis-sites" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Sites (Old)</a></li>
-                        <li><a href="https://drive.google.com/drive/folders/1gAMLO2AdXqsp75ymexKwXllKbLMTfhxa" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Archives</a></li>
-                        <li><a href="<?php echo $pathToWebRoot.'/pages/networks.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Group Network Lists</a></li>
-                    </ul>
-                </nav>
-            </section>
-            <section class="quick-link">
-                <section class="footer-title mb-6 text-xl sm:text-sm lg:text-lg font-semibold text-black uppercase">Quick Link</section>
-                <nav>
-                    <ul class="mb-5">
-                        <li><a href="https://inzaghiposuma.wordpress.com/blog-posts" class="text-gray-800 hover:text-blue-800" target="_blank">WordPress Blog</a></li>
-                        <li><a href="https://medium.com/@izzumiposhaf29" class="text-gray-800 hover:text-blue-800">Inzaghi's Sites Blog</a></li>
-                        <li><a href="https://linktr.ee/inzaghigroup" class="text-gray-800 hover:text-blue-800" target="_blank">Linktree Inzaghi's Group</a></li>
-                        <li><a href="https://old.inzaghi.wuaze.com" class="font-semibold text-green-900 hover:text-green-600"><b>Back to Old UI (Legacy)</b></a></li>
-                    </ul>
-                    <a class="github-button" href="https://github.com/inzaghidev/Inzaghis-Sites" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Look at @inzaghidev">Look at @inzaghidev</a>
-                </nav>
+            <section class="footer-grid mx-0 lg:mx-2 mb-5 md:mb-6 lg:mb-2">
+                <section class="footer-list">
+                    <section class="footer-title mb-6 text-xl font-semibold text-black uppercase">Special Pages</section>
+                    <nav>
+                        <ul>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/about.php'; ?>" class="text-gray-800 hover:text-blue-800">About</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/disclaimer.php'; ?>" class="text-gray-800 hover:text-blue-800">Disclaimer</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/privacy-policy.php'; ?>" class="text-gray-800 hover:text-blue-800">Privacy Policy</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/contact.php'; ?>" class="text-gray-800 hover:text-blue-800">Contact</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/portals/'; ?>" class="text-gray-800 hover:text-blue-800">Portals</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/page-apps/'; ?>" class="text-gray-800 hover:text-blue-800">Apps</a></li>
+                            <li><a href="https://medium.com/@izzumiposhaf29" class="text-gray-800 hover:text-blue-800" target="_blank">Blog</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/sitemap.php'; ?>" class="text-gray-800 hover:text-blue-800">Sitemap</a></li>
+                        </ul>
+                    </nav>
+                </section>
+                <section class="footer-list">
+                    <section class="footer-title mb-6 text-xl font-semibold text-black uppercase">Portals</section>
+                    <nav>
+                        <ul>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/widgets.php'; ?>" class="text-gray-800 hover:text-blue-800">Widgets</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/tech-tutorials.php'; ?>" class="text-gray-800 hover:text-blue-800">Technology Tutorials</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/projects.php'; ?>" class="text-gray-800 hover:text-blue-800">IT Project Lists</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/interviews-and-thesis.php'; ?>" class="text-gray-800 hover:text-blue-800">Interviews & Thesis Prep</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/learning-materials.php'; ?>" class="text-gray-800 hover:text-blue-800">Learning Materials</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/video-pages.php'; ?>" class="text-gray-800 hover:text-blue-800">Video Pages</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/link-pages.php'; ?>" class="text-gray-800 hover:text-blue-800">Link Pages</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/miscellaneous.php'; ?>" class="text-gray-800 hover:text-blue-800">Miscellaneous</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/portals/'; ?>" class="text-gray-800 hover:text-blue-800">Portal Lists</a></li>
+                        </ul>
+                    </nav>
+                </section>
+                <section class="footer-list">
+                    <section class="footer-title mb-6 text-xl font-semibold text-black uppercase">Apps & Tools</section>
+                    <nav>
+                        <ul>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/converter.php'; ?>" class="text-gray-800 hover:text-blue-800">Converters</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/calculators.php'; ?>" class="text-gray-800 hover:text-blue-800">Calculators</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/generators.php'; ?>" class="text-gray-800 hover:text-blue-800">Generators</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/formatters.php'; ?>" class="text-gray-800 hover:text-blue-800">Formatters</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/file-converter.php'; ?>" class="text-gray-800 hover:text-blue-800">File Converter</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/utilities.php'; ?>" class="text-gray-800 hover:text-blue-800">Utilities</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/tester-tools.php'; ?>" class="text-gray-800 hover:text-blue-800">Tester Tools</a></li>
+                        <li><a href="<?php echo $pathToWebRoot.'/page-apps/'; ?>" class="text-gray-800 hover:text-blue-800">Other Tools</a></li>
+                        </ul>
+                    </nav>
+                </section>
+                <section class="footer-list">
+                    <section class="footer-title mb-6 text-xl font-semibold text-black uppercase">Networks</section>
+                    <nav>
+                        <ul>
+                            <li><a href="<?php echo $pathToWebRoot.'/inzaghis-group/inzaghis-blog.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Blog</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/inzaghis-group/inzaghis-media.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Media</a></li>
+                            <li><a href="https://github.com/inzaghidev" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Dev (GitHub)</a></li>
+                            <li><a href="https://sites.google.com/view/inzaghis-sites" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Sites (Old)</a></li>
+                            <li><a href="https://drive.google.com/drive/folders/1gAMLO2AdXqsp75ymexKwXllKbLMTfhxa" class="text-gray-800 hover:text-blue-800" target="_blank">Inzaghi's Archives</a></li>
+                            <li><a href="<?php echo $pathToWebRoot.'/pages/networks.php'; ?>" class="text-gray-800 hover:text-blue-800">Inzaghi's Group Network Lists</a></li>
+                        </ul>
+                    </nav>
+                </section>
+                <section class="footer-list">
+                    <section class="footer-title mb-6 text-xl font-semibold text-black uppercase">Quick Link</section>
+                    <nav>
+                        <ul class="mb-5">
+                            <li><a href="https://inzaghiposuma.wordpress.com/blog-posts" class="text-gray-800 hover:text-blue-800" target="_blank">WordPress Blog</a></li>
+                            <li><a href="https://medium.com/@izzumiposhaf29" class="text-gray-800 hover:text-blue-800">Inzaghi's Sites Blog</a></li>
+                            <li><a href="https://linktr.ee/inzaghigroup" class="text-gray-800 hover:text-blue-800" target="_blank">Linktree Inzaghi's Group</a></li>
+                            <li><a href="https://old.inzaghi.wuaze.com" class="font-semibold text-green-900 hover:text-green-600"><b>Back to Old UI (Legacy)</b></a></li>
+                        </ul>
+                        <a class="github-button" href="https://github.com/inzaghidev/Inzaghis-Sites" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Look at @inzaghidev">Look at @inzaghidev</a>
+                    </nav>
+                </section>
             </section>
         </section>
         <section class="footer-bottom">
