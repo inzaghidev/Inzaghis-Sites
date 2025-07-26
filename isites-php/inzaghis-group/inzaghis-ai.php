0e209ea6364b676119c4adf89f16a8df0aefdb06
inzaghidev <izzumiposhaf29@gmail.com>
2025-07-26 19:26:04 +0700

    Add some Pages on Inzaghi's Group

---
+++ isites-php/inzaghis-group/inzaghis-ai.php
@@ -0,0 +1,21 @@
+<?php
+	$page_title = "Inzaghi's AI";
+
+  include_once __DIR__ . '/../includes/config.php';
+  include('../layouts/header.php');
+  include('../components/navbar/navbar.php');
+?>
+    <div class="container-top"></div>
+    <section class="inzaghis-group-pages inzaghisai-slider">
+      <div class="page-wrapper inzagroup-pages">
+        <h1 class="my-4 page-title">Inzaghi's AI</h1>
+        <div
+          class="bg-white shadow-xl rounded-lg p-6 ring-1 ring-yellow-700 p-6"
+        >
+        </div>
+      </div>
+    </section>
+<?php
+	include('../components/footsite/footsite.php');
+  include('../layouts/footer.php');
+?>
---
+++ isites-php/inzaghis-group/inzaghis-app.php
@@ -0,0 +1,21 @@
+<?php
+	$page_title = "Inzaghi's App";
+
+  include_once __DIR__ . '/../includes/config.php';
+  include('../layouts/header.php');
+  include('../components/navbar/navbar.php');
+?>
+    <div class="container-top"></div>
+    <section class="inzaghis-group-pages inzaghisapp-slider">
+      <div class="page-wrapper inzagroup-pages">
+        <h1 class="my-4 page-title">Inzaghi's App</h1>
+        <div
+          class="bg-white shadow-xl rounded-lg p-6 ring-1 ring-blue-400 p-6"
+        >
+        </div>
+      </div>
+    </section>
+<?php
+	include('../components/footsite/footsite.php');
+  include('../layouts/footer.php');
+?>
