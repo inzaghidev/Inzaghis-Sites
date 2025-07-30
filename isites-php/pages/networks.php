4f7a18b6865fcb277e597b188e77ef6e780cc262
inzaghidev <izzumiposhaf29@gmail.com>
2025-07-30 22:45:26 +0700

    Edited some Pages on Inzaghi's Group and Networks

--- isites-php/pages/networks.php
+++ isites-php/pages/networks.php
@@ -377,7 +377,7 @@
             Inzaghi's App akan tersedia dalam Versi Android dan iOS.
           </p>
           <section class="group-button">
-            <a href="#" class="main-button">Click here</a>
+            <a href="../inzaghis-group/inzaghis-app.php" class="main-button">Click here</a>
           </section>
         </section>
       </section>
@@ -436,7 +436,7 @@
             AI-API Key apapun.
           </p>
           <section class="group-button">
-            <a href="#" class="main-button">Click here</a>
+            <a href="../inzaghis-group/inzaghis-ai.php" class="main-button">Click here</a>
           </section>
         </section>
       </section>
