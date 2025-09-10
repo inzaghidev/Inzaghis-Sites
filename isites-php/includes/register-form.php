1fbd1d6a89dbdde6dc22a982639efe53ebd68dfd
inzaghidev <izzumiposhaf29@gmail.com>
2025-09-10 23:44:17 +0700

    Edited some Files in Login & Registration Form

--- isites-php/includes/register-form.php
+++ isites-php/includes/register-form.php
@@ -1,5 +1,5 @@
 <?php
-	$page_title = "Home";
+	$page_title = "Registration Form";
 	include('../layouts/header.php');
     include('../components/navbar/navbar.php');
 ?>
@@ -8,7 +8,7 @@
         <form>
         <div class="container">
             <center>
-            <h1>Registeration Form</h1>
+            <h1>Registration Form</h1>
             </center>
             <hr>
             <label>Firstname : </label>
