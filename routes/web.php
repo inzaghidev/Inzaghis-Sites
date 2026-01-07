40281d290533ceb268589bd59dbd4b194b095752
inzaghidev <izzumiposhaf29@gmail.com>
2026-01-07 15:01:52 +0700

    Edited some Files in web.php and also in Apps & Portals Pages

--- routes/web.php
+++ routes/web.php
@@ -3,43 +3,61 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('/index');
+    return view('/index', [
+        'page_title' => 'Home'
+    ]);
 });
 
 Route::get('/public', function () {
-    return view('/index');
+    return view('/index', [
+        'page_title' => 'Home'
+    ]);
 });
 
 Route::get('/about', function () {
-    return view('/pages/about');
+    return view('/pages/about', [
+        'page_title' => 'About'
+    ]);
 });
 
 Route::get('/profile', function () {
-    return view('/pages/profile');
+    return view('/pages/profile', [
+        'page_title' => 'Profile'
+    ]);
 });
 
 Route::get('/contact', function () {
-    return view('/pages/contact');
+    return view('/pages/contact', [
+        'page_title' => 'Contact'
+    ]);
 });
 
 Route::get('/networks', function () {
-    return view('/pages/networks');
+    return view('/pages/networks', [
+        'page_title' => "Inzaghi's Group Network"
+    ]);
 });
 
 Route::get('/disclaimer', function () {
-    return view('/pages/disclaimer');
+    return view('/pages/disclaimer', [
+        'page_title' => 'Disclaimer'
+    ]);
 });
 
 Route::get('/privacy-policy', function () {
-    return view('/pages/privacy-policy');
+    return view('/pages/privacy-policy', [
+        'page_title' => 'Privacy Policy'
+    ]);
 });
 
 Route::get('/apps', function () {
-    return view('/pages/apps');
+    return view('/apps/index', [
+        'page_title' => 'Apps'
+    ]);
 });
 
 Route::get('/portals', function () {
-    return view('/pages/portals');
+    return view('/portals/index');
 });
 
 
