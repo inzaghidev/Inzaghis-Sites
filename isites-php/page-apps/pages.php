6eb96250a54fe186475c59f39c633747708f17f2
inzaghidev <izzumiposhaf29@gmail.com>
2026-02-12 23:59:49 +0700

    Edited some Files & Improved UI on Pages (Apps)

--- isites-php/page-apps/pages.php
+++ isites-php/page-apps/pages.php
@@ -1,14 +1,52 @@
+<!-- UNDER CONSTRUCTION! -->
+
 <?php
   $page_title = "Pages";
   include('../layouts/header.php');
   include('../components/navbar/navbar.php');
   $data = json_decode(file_get_contents('../data/apps-data.json'), true);
 
-  if (isset($data['pages'])) {
-      $apps = $data['pages'];
-  } else {
-      $apps = []; // Atau tangani kesalahan dengan lebih baik sesuai kebutuhan Anda
-  }
+  $sections = [
+    [
+      "section" => "Section 1",
+      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "icon" => "../assets/icons/ico-icon.svg",
+      "apps" => [
+        [
+          "title" => "App 1",
+          "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "link" => "#"
+        ],
+        [
+          "title" => "App 2",
+          "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "link" => "#"
+        ],
+        [
+          "title" => "App 3",
+          "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "link" => "#"
+        ]
+      ]
+    ],
+    [
+      "section" => "Section 2",
+      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "icon" => "../assets/icons/ico-icon.svg",
+      "apps" => [
+        [
+          "title" => "App 1",
+          "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "link" => "#"
+        ],
+        [
+          "title" => "App 2",
+          "text" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "link" => "#"
+        ]
+      ]
+    ]
+  ];
 ?>
 
 <div class="container-top"></div>
@@ -42,25 +80,36 @@
             echo $foundDescription;
           ?>
         </p>
-        <div class="card-grid-container">
-          <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 -mx-4 sm:mx-0">
-          <?php
-            foreach ($apps as $app) {
-              $title = isset($app['title']) ? $app['title'] : 'N/A';
-              $text = isset($app['text']) ? $app['text'] : '';
-              $link = isset($app['link']) ? $app['link'] : '#';
-
-              echo '<div class="bg-white hover:bg-[#edf5ef] shadow-md flex flex-col justify-between rounded-lg p-6 ring-1 ring-green-500 border border-indigo-100 rounded-lg shadow hover:shadow-xl duration-500">';
-              echo '  <h5 class="card-apps-title text-2xl font-bold text-black">' . $title . '</h5>';
-              echo '  <p class="card-apps-text text-gray-600 mt-2 !mb-4 mb-auto">' . $text . '</p>';
-              echo '  <a href="' . $link . '" class="flex justify-center bg-blue-600 text-white mx-2 py-2 px-4 rounded-lg hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 shadow hover:shadow-lg">';
-              echo '    <button class="inline-block">Click here</button>';
-              echo '  </a>';
-              echo '</div>';
-            }
-          ?>
+        <?php foreach ($sections as $section): ?>
+          <div class="card-apps bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-30 border border-gray-100 shadow-xl flex flex-col justify-between p-6 mb-8 ring-1 ring-green-500">
+            <div class="card-grid-container">
+              <div class="flex justify-between flex-wrap gap-2 mb-4 w-full">
+                <div class="flex items-center">
+                  <img src="<?= $section['icon'] ?>" width="44" height="44" />
+                  <span class="text-3xl ml-3 text-gray-700 font-bold font-[Inter]">
+                    <?= $section['section'] ?>
+                  </span>
+                </div>
+              </div>
+              <div class="text-container">
+                <p class="is-desc">
+                  <?= $section['description'] ?>
+                </p>
+              </div>
+              <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                <?php
+                  foreach ($section['apps'] as $app) {
+                    echo '<div class="bg-white hover:bg-green-50 shadow-md flex flex-col justify-between rounded-lg p-6 ring-1 ring-green-500 border border-indigo-100 rounded-lg shadow hover:shadow-xl transition-all transition transform hover:-translate-y-1 duration-500">';
+                    echo '  <h5 class="card-apps-title text-2xl font-bold text-black">' . $app["title"] . '</h5>';
+                    echo '  <p class="card-apps-text text-gray-600 mt-2 !mb-4 mb-auto">' . $app["text"] . '</p>';
+                    echo '  <a href="' . $app["link"] . '" class="flex justify-center bg-blue-600 text-white mx-2 py-2 px-4 rounded-lg hover:bg-blue-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 shadow hover:shadow-lg">Click here</a>';
+                    echo '</div>';
+                  }
+                ?>
+              </div>
+            </div>
           </div>
-        </div>
+        <?php endforeach ?>
       </section>
     </div>
   </div>
