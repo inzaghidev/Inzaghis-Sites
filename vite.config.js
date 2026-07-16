8a0a98237c90969f058e31b6e0141cc3e443d61c
inzaghidev <izzumiposhaf29@gmail.com>
2026-07-16 19:30:15 +0700

    Edited some Files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": "/src",
